f0eadeb4fed566c44f6093379bbd3104a1e16776
Eli Thorkelson <eli@uchicago.edu>
2018-11-20 18:29:08 -0500

    Add nicer failure messages

--- export.rb
+++ export.rb
@@ -9,6 +9,11 @@ def ok(message)
   puts "✅ #{message}"
 end
 
+def die(message)
+  puts "❌ #{message}"
+  abort
+end
+
 # Load settings
 settings = YAML.load_file 'settings.yml'
 
@@ -18,24 +23,26 @@ Dir.mktmpdir do |tmp_dir|
 
   # Run Scrivener export
   output = `osascript scrivener-export.applescript #{tmp_dir}`
-  abort("AppleScript output: #{output}") unless output.strip.empty?
-  abort("AppleScript failed for some reason: #{output}") unless $?.success?
-  ok "Successfully exported plain text from Scrivener"
+  die("AppleScript output: #{output}") unless output.include?('/OK')
+  die("AppleScript failed for some reason: #{output}") unless $?.success?
 
   # Find latest export folder
   latest_export_path = Dir.glob("#{tmp_dir}/*").first
 
   # Export folder should be a directory
-  abort("Latest export is not a directory") unless File.directory? latest_export_path
+  unless File.directory? latest_export_path
+    puts "Contents of exported directory:", Dir.entries(tmp_dir)
+    die("Latest export is not a directory")
+  end
 
   # Look up repo path for this project
   project_name = latest_export_path.split('|').last
   repo_path = settings['repo_paths'][project_name]
-  abort("No repo path found for #{project_name}") unless repo_path
+  die("No repo path found for #{project_name}") unless repo_path
 
   # Verify that the repo has a content directory
   dest_path = "#{repo_path}/content"
-  abort("Repo #{repo_path} has no content directory") unless File.directory? dest_path
+  die("Repo #{repo_path} has no content directory") unless File.directory? dest_path
 
   # Remove original (easiest way to handle any removed files in the src folder)
   ok "Removing #{dest_path}"
